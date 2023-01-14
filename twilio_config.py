6d4ee11a22ad97edf3071f6caab7d00f1b8545d5
Chakyll Calderon <chakyllfcu@gmail.com>
2023-01-14 14:25:46 -0400

    first commit

---
+++ twilio_config.py
@@ -0,0 +1,4 @@
+TWILIO_ACCOUNT_SID ='abcdabdcabcdabdcabcdabdcabcdabdc'
+TWILIO_AUTH_TOKEN ='abcdabdcabcdabdcabcdabdcabcdabdc'
+PHONE_NUMBER ='+123456789101112'
+API_KEY_WAPI = 'abcdabdcabcdabdcabcdabdcabcdabdcab'
